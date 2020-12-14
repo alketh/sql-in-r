02401927c624fa2b69c6f0129eef3d8bd3a9a2e4
Alexander Keth <alexander.keth@uni-hamburg.de>
2020-12-14 22:45:47 +0100

    Simple 3 tbl join.

--- join.R
+++ join.R
@@ -13,4 +13,18 @@ tbl <- left_join(con1, con2, by = "tailnum")
 
 show_query(tbl)
 
+# 3 tbl join query!
+all(unique(nycflights13::flights$origin) %in% unique(nycflights13::airports$faa))
+
+copy_to(con, nycflights13::airports, "airports", temporary = FALSE)
+
+con3 <- tbl(con, "airports")
+
+chk <- left_join(con1, con2, by = "tailnum") %>%
+  left_join(con3, by = c("origin" = "faa")) %>%
+  group_by(origin) %>%
+  summarise(count = n())
+
+collect(chk)
+
 
